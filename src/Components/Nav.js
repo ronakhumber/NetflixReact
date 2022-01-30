068438880a822d311b6023d9e4692433fc48d4e5
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2022-01-29 22:15:24 -0800

    Convert to class based component

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,16 +1,20 @@
 import netflixlogo from './../img/netflix.png'
+import React from 'react';
 
-function Nav() {
-    return ( 
-    <nav className="navigation">
-    <article className="logo-wrapper">
-    <img src={netflixlogo} alt="Netflix" />
-    </article>
-    <button className="sign-in-btn">
-    Sign In
-    </button>
-    </nav>
-     );
+class Nav extends React.Component {
+    state = {  } 
+    render() { 
+        return (
+            <nav className="navigation">
+            <article className="logo-wrapper">
+            <img src={netflixlogo} alt="Netflix" />
+            </article>
+            <button className="sign-in-btn">
+            Sign In
+            </button>
+            </nav>
+        );
+    }
 }
-
+ 
 export default Nav;
